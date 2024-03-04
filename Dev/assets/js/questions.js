c801e5969526703fd34d64aa7e45c1ea91a48ffe
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-03-03 19:26:54 -0800

    compiled list of questions for quiz from W3 schools

---
+++ Dev/assets/js/questions.js
@@ -0,0 +1,44 @@
+// All of the questions, choices, and answer key
+//Quiz questions referenced from my independent study on W3 Schools 
+const questions = [
+    {
+        title: 'Inside which HTML element do we store our JavaScript?',
+        choices: ['<js>', '<javascript>', '<script>', '<link>'],
+        answer: '<script>',
+    },
+    {
+        title: 'Which operator is used too assign a value to a variable?',
+        choices: ['x', '*', 'myVar', '='],
+        answer: '=',
+    },
+    {
+        title: 'What is the correct way to write a Javascript array?',
+        choices: [
+            'var coffee = (1:"beans", 2:"arabica", 3:"sumatra")',
+            'var coffee = "beans", "arabica", "sumatra"',
+            'var coffee = 1 = ("beans"), 2 = ("arabica"), 3 = ("sumatra")',
+            'var coffee = ["beans", "arabica", "sumatra"]',  
+        ],
+        answer: '["beans", "arabica", "sumatra"]',
+    },
+    {
+        title: 'How do you write "Hello World" in an alert box?',
+        choices: [
+            'alert("Hello World");',
+            'msgBox("Hello World");',
+            'msg("Hello World");',
+            'alertBox("Hello World");',
+        ],
+        answer: 'alert("Hello World");',
+    },
+    {
+        title: 'How does a FOR loop start?',
+        choices: [
+            'for (i <= 5; i++)',
+            'for (i = 0; i <= 5)',
+            'for (i = 0; i <= 5; i++)',
+            'for i = 1 to 5',
+        ],
+        answer: 'for (i = 0; i <= 5; i++)'
+    },
+];
